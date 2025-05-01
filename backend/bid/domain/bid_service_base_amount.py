765e858270ee2e849fb6a90b5a974d86e08394a8
jaeho <jaeho.lim@sk.com>
2025-05-02 00:59:20 +0900

    데이터 클래스 추가 중

---
+++ backend/bid/domain/bid_service_base_amount.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from pydantic import BaseModel
+from dataclasses import dataclass
+from backend.bid.domain import Bid
+
+
+# 용역기초금액조회
+@dataclass
+class BidServiceBaseAmount(Bid):
+    bid_ntce_no: Optional[str] = None  # 입찰공고번호
+    bid_ntce_ord: Optional[str] = None  # 입찰공고차수
+    bid_clsfc_no: Optional[str] = None  # 입찰분류번호
+    bid_ntce_nm: Optional[str] = None  # 입찰공고명
+    bssamt: Optional[str] = None  # 기초금액
+    bssamt_open_dt: Optional[str] = None  # 기초금액공개일시
+    rsrvtn_prce_rng_bgn_rate: Optional[str] = None  # 예비가격범위시작률
+    rsrvtn_prce_rng_end_rate: Optional[str] = None  # 예비가격범위종료율
+    evl_bss_amt: Optional[str] = None  # 평가기준금액
+    dfcltydgr_cfcnt: Optional[str] = None  # 난이도계수
+    etc_gnrlexpns_bss_rate: Optional[str] = None  # 기타경비기준율
+    gnrl_mngcst_bss_rate: Optional[str] = None  # 일반관리비기준율
+    prft_bss_rate: Optional[str] = None  # 이윤기준율
+    lbrcst_bss_rate: Optional[str] = None  # 노무비기준율
+    sfty_mngcst: Optional[str] = None  # 산업안전보건관리비
+    rtrfund_non: Optional[str] = None  # 퇴직공제부금비
+    env_cnsrvcst: Optional[str] = None  # 환경보전비
+    scontrct_payprce_pay_grnty_fee: Optional[str] = None  # 하도급대금지급보증수수료
+    mrfn_health_insrprm: Optional[str] = None  # 국민건강보험료
+    npn_insrprm: Optional[str] = None  # 국민연금보험료
+    rmrk1: Optional[str] = None  # 비고1
+    rmrk2: Optional[str] = None  # 비고2
+    odsn_lngtrmrcpr_insrprm: Optional[str] = None  # 노인장기요양보험료
+    useful_amt: Optional[str] = None  # 가용금액
+    inpt_dt: Optional[str] = None  # 입력일시
+    sfty_chck_mngcst: Optional[str] = None  # 안전관리비
---
+++ backend/bid/infra/models/bid_service_base_amount.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import String, Text
+from backend.database import Base
+
+
+# 용역기초금액조회
+class BidServiceBaseAmount(Base):
+    __tablename__ = 'bid_service_base_amount'
+
+    bid_ntce_no = mapped_column(String(40), comment='입찰공고번호')
+    bid_ntce_ord = mapped_column(String(3), comment='입찰공고차수')
+    bid_clsfc_no = mapped_column(String(5), comment='입찰분류번호')
+    bid_ntce_nm = mapped_column(String(100), comment='입찰공고명')
+    bssamt = mapped_column(String(21), comment='기초금액')
+    bssamt_open_dt = mapped_column(String(19), comment='기초금액공개일시')
+    rsrvtn_prce_rng_bgn_rate = mapped_column(String(7), comment='예비가격범위시작률')
+    rsrvtn_prce_rng_end_rate = mapped_column(String(7), comment='예비가격범위종료율')
+    evl_bss_amt = mapped_column(String(21), comment='평가기준금액')
+    dfcltydgr_cfcnt = mapped_column(String(5), comment='난이도계수')
+    etc_gnrlexpns_bss_rate = mapped_column(String(8), comment='기타경비기준율')
+    gnrl_mngcst_bss_rate = mapped_column(String(8), comment='일반관리비기준율')
+    prft_bss_rate = mapped_column(String(8), comment='이윤기준율')
+    lbrcst_bss_rate = mapped_column(String(8), comment='노무비기준율')
+    sfty_mngcst = mapped_column(String(17), comment='산업안전보건관리비')
+    rtrfund_non = mapped_column(String(17), comment='퇴직공제부금비')
+    env_cnsrvcst = mapped_column(String(15), comment='환경보전비')
+    scontrct_payprce_pay_grnty_fee = mapped_column(String(21), comment='하도급대금지급보증수수료')
+    mrfn_health_insrprm = mapped_column(String(18), comment='국민건강보험료')
+    npn_insrprm = mapped_column(String(18), comment='국민연금보험료')
+    rmrk1 = mapped_column(Text, comment='비고1')
+    rmrk2 = mapped_column(Text, comment='비고2')
+    odsn_lngtrmrcpr_insrprm = mapped_column(String(18), comment='노인장기요양보험료')
+    useful_amt = mapped_column(String(21), comment='가용금액')
+    inpt_dt = mapped_column(String(19), comment='입력일시')
+    sfty_chck_mngcst = mapped_column(String(17), comment='안전관리비')
