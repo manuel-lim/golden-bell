731344c8762ae964aa21f836e6fd4a75dc7a9f78
jaeho <jaeho.lim@sk.com>
2025-05-10 01:10:32 +0900

    낙찰 정보 추가

---
+++ backend/win_bid/domain/win_bid_open_construction_status.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+from dataclasses import dataclass
+
+
+@dataclass
+class WinBidConstructionStatus(BaseModel):
+    id: Optional[int] = None
+    bid_ntce_no: Optional[str] = None
+    bid_ntce_ord: Optional[str] = None
+    bid_clsfc_no: Optional[str] = None
+    rbid_no: Optional[str] = None
+    bid_ntce_nm: Optional[str] = None
+    openg_dt: Optional[str] = None
+    prtcpt_cnum: Optional[str] = None
+    openg_corp_info: Optional[str] = None
+    progrs_div_cd_nm: Optional[str] = None
+    inpt_dt: Optional[str] = None
+    rsrvtn_prce_file_existnce_yn: Optional[str] = None
+    ntce_instt_cd: Optional[str] = None
+    ntce_instt_nm: Optional[str] = None
+    dminstt_cd: Optional[str] = None
+    dminstt_nm: Optional[str] = None
+    openg_rslt_ntc_cntnts: Optional[str] = None
+    created_at: Optional[str] = None
---
+++ backend/win_bid/infra/models/win_bid_open_construction_status.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String, BigInteger, DateTime, text
+from backend.database import Base
+
+
+class WinBidConstructionStatus(Base):
+    __tablename__ = 'win_bid_construction_status'
+    __table_args__ = {'comment': '개찰결과 공사 목록 조회'}
+
+    id = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    bid_ntce_no = mapped_column(String(11), comment='입찰공고번호')
+    bid_ntce_ord = mapped_column(String(3), comment='입찰공고차수')
+    bid_clsfc_no = mapped_column(String(5), comment='입찰분류번호')
+    rbid_no = mapped_column(String(3), comment='재입찰번호')
+    bid_ntce_nm = mapped_column(String(100), comment='입찰공고명')
+    openg_dt = mapped_column(String(19), comment='개찰일시')
+    prtcpt_cnum = mapped_column(String(6), comment='참가업체수')
+    openg_corp_info = mapped_column(String(500), comment='개찰업체정보')
+    progrs_div_cd_nm = mapped_column(String(4), comment='진행구분코드명')
+    inpt_dt = mapped_column(String(19), comment='입력일시')
+    rsrvtn_prce_file_existnce_yn = mapped_column(String(1), comment='예비가격파일존재여부')
+    ntce_instt_cd = mapped_column(String(7), comment='공고기관코드')
+    ntce_instt_nm = mapped_column(String(200), comment='공고기관명')
+    dminstt_cd = mapped_column(String(7), comment='수요기관코드')
+    dminstt_nm = mapped_column(String(200), comment='수요기관명')
+    openg_rslt_ntc_cntnts = mapped_column(String(4000), comment='개찰결과공지내용')
+    created_at = mapped_column(DateTime, server_default=text('CURRENT_TIMESTAMP'))
