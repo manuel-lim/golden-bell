9a16407e8e841995a2453bfbce8d168049f4867b
jaeho <jaeho.lim@sk.com>
2025-05-10 01:38:55 +0900

    낙찰 정보 추가

---
+++ backend/win_bid/domain/win_bid_open_completed.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+from dataclasses import dataclass
+
+
+@dataclass
+class WinBidOpenCompleted(BaseModel):
+    id: Optional[int] = None
+    openg_rslt_div_nm: Optional[str] = None
+    bid_ntce_no: Optional[str] = None
+    bid_ntce_ord: Optional[str] = None
+    bid_clsfc_no: Optional[str] = None
+    rbid_no: Optional[str] = None
+    openg_rank: Optional[str] = None
+    prcbdr_bizno: Optional[str] = None
+    prcbdr_nm: Optional[str] = None
+    prcbdr_ceo_nm: Optional[str] = None
+    bidprc_amt: Optional[str] = None
+    bidprcrt: Optional[str] = None
+    rmrk: Optional[str] = None
+    cnstty_accot_bid_amt_url: Optional[str] = None
+    drwt_no1: Optional[str] = None
+    drwt_no2: Optional[str] = None
+    bidprc_dt: Optional[str] = None
+    created_at: Optional[str] = None
---
+++ backend/win_bid/infra/models/win_bid_open_completed.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String, BigInteger, DateTime, text
+from backend.database import Base
+
+
+class WinBidOpenCompleted(Base):
+    __tablename__ = 'win_bid_open_completed'
+    __table_args__ = {'comment': '개찰결과 개찰완료 목록 조회'}
+
+    id = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+
+    openg_rslt_div_nm = mapped_column(String(30), comment='개찰결과구분명')
+    bid_ntce_no = mapped_column(String(11), comment='입찰공고번호')
+    bid_ntce_ord = mapped_column(String(3), comment='입찰공고차수')
+    bid_clsfc_no = mapped_column(String(5), comment='입찰분류번호')
+    rbid_no = mapped_column(String(3), comment='재입찰번호')
+    openg_rank = mapped_column(String(7), comment='개찰순위')
+    prcbdr_bizno = mapped_column(String(10), comment='투찰업체사업자등록번호')
+    prcbdr_nm = mapped_column(String(100), comment='투찰업체명')
+    prcbdr_ceo_nm = mapped_column(String(35), comment='투찰업체대표자명')
+    bidprc_amt = mapped_column(String(21), comment='투찰금액')
+    bidprcrt = mapped_column(String(18), comment='투찰률')
+    rmrk = mapped_column(String(200), comment='비고')
+    cnstty_accot_bid_amt_url = mapped_column(String(150), comment='공종별입찰금액URL')
+    drwt_no1 = mapped_column(String(3), comment='추첨번호1')
+    drwt_no2 = mapped_column(String(3), comment='추첨번호2')
+    bidprc_dt = mapped_column(String(19), comment='투찰일시')
+    created_at = mapped_column(DateTime, server_default=text('CURRENT_TIMESTAMP'))
