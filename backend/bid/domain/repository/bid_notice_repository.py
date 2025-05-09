34e03beeef6da0121dfef0a5f519dcbd831a8692
jaeho <jaeho.lim@sk.com>
2025-05-09 16:03:35 +0900

    현재 개발 진행 커밋

--- backend/bid/domain/repository/bid_notice_repository.py
+++ backend/bid/domain/repository/bid_notice_repository.py
@@ -10,8 +10,3 @@ class IBidNoticeRepository(metaclass=ABCMeta):
     @abstractmethod
     def get_bid_construction_list(self, page, per_page, body_data: dict) -> tuple[int, list[BidConstructionDTO]]:
         raise NotImplementedError
-
-
-    @abstractmethod
-    def get_bid_construction(self, bid_construction_id: str) -> BidConstructionDTO:
-        raise NotImplementedError
